6b3e0810c04531fe09bde1867bd7ac80b2f2034b
GHOST0028 <raunaksingh1065@gmail.com>
2026-03-31 00:28:04 +0530

    Initial commit - Moodzy project

---
+++ script.js
@@ -0,0 +1,15 @@
+function goStart() {
+  window.location.href = "start.html";
+}
+
+function goGame() {
+  window.location.href = "play.html";
+}
+
+function goMusic() {
+  window.location.href = "relax.html";
+}
+
+function goCalm() {
+  window.location.href = "calm.html";
+}
